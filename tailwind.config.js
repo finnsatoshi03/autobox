cc405116dd82b0edaed71764f9c03bd41cf60385
finnsatoshi03 <finnsatoshi03@gmail.com>
2024-12-21 14:04:55 +0800

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  darkMode: ["class"],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Neue Haas Grotesk Display Pro", "sans-serif"],
+        pixel: ["Pixelify Sans", "sans-serif"],
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+      colors: {
+        black: "#171717",
+        "neon-pink": "#f712e8",
+        "lime-green": "#39e717",
+      },
+    },
+  },
+  plugins: [require("tailwindcss-animate")],
+};
